70b6a74b050a7c84c4ba6768ac67d02f81dee95b
BegzodDev <begzodpublic@gmail.com>
2023-01-25 11:07:28 +0500

    sss

--- CheckGroup/CheckGroup/Data/Models/Teacher.cs
+++ CheckGroup/CheckGroup/Data/Models/Teacher.cs
@@ -2,8 +2,13 @@
 {
     public class Teacher
     {
+
         public int Id { get; set; }
+
         public string Name { get; set; }
 
+        public string Passwordhash { get; set; }
+
+        public Group Groups { get; set; }
     }
 }
